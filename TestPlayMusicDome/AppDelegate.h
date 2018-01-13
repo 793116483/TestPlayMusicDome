f0bb4ce11219637a5b45321b3a0ab848f20595ab
qujie <793116483@qq.com>
2018-01-13 14:09:07 +0800

    Initial Commit

---
+++ TestPlayMusicDome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestPlayMusicDome
+//
+//  Created by qujie on 2018/1/13.
+//  Copyright © 2018年 linkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestPlayMusicDome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestPlayMusicDome
+//
+//  Created by qujie on 2018/1/13.
+//  Copyright © 2018年 linkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
